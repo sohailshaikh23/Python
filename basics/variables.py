0551633fc7fd7ea9b4987af730584f03db2c025b
sohailshaikh23 <136561120+sohailshaikh23@users.noreply.github.com>
2026-08-08 14:37:13 +0530

    Update variables.py

--- basics/variables.py
+++ basics/variables.py
@@ -1,5 +1,22 @@
-# Valid variable names
+# Variables
 name = "John"
 age = 25
 user_name = "John"
 student1 = "Alex"
+
+
+#constants
+PI = 3.14159
+MAX_USERS = 100
+COMPANY_NAME = "Google"
+
+#check data types
+name = "Rahul"
+age = 20
+height = 5.8
+is_student = True
+
+print(type(name))    # it will give str 
+print(type(age))    # it will give int
+print(type(height)) # it will give float
+print(type(is_student))    # it will give bool
